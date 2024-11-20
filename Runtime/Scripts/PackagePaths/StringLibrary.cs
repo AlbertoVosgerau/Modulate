b0a5d97d7971fdc028396831248d7b79cb088743
AlbertoVosgerau <alberto.vosgerau@ringtailinteractive.com>
2024-11-20 19:37:47 +0100

    Fixed Modulate! Menuitems

--- Runtime/Scripts/PackagePaths/StringLibrary.cs
+++ Runtime/Scripts/PackagePaths/StringLibrary.cs
@@ -10,13 +10,13 @@ namespace DandyDino.Modulate
         public const string MANAGER_CONTAINER_NAME = "MangerContainer";
         public const string SERVICES_COLLECTION_NAME = "ServicesCollection";
         
-        public const string MODULATE_WINDOW = "Dandy Dino/Core/Open Modulate Window %&#c";
-        public const string CODE_FACTORY_WINDOW = "Dandy Dino/Modulate/Code Factory";
-        public const string SMART_NAVIGATOR = "Dandy Dino/Modulate/Smart Navigator";
+        public const string MODULATE_WINDOW = "Dandy Dino/Modulate!/Open Modulate Window %&#c";
+        public const string CODE_FACTORY_WINDOW = "Dandy Dino/Modulate!/Code Factory";
+        public const string SMART_NAVIGATOR = "Dandy Dino/Modulate!/Smart Navigator";
 
-        public const string GO_CREATE_MANAGERS_GAMEOBJECT = "GameObject/Dandy Dino/Modulate/Create Manager Container";
+        public const string GO_CREATE_MANAGERS_GAMEOBJECT = "GameObject/Dandy Dino/Modulate!/Create Manager Container";
 
-        public const string CREATE_GAME = "Assets/Create/Dandy Dino/Modulate/Create New Game";
-        public const string CREATE_MODULE = "Assets/Create/Dandy Dino/Modulate/Create New Module";
+        public const string CREATE_GAME = "Assets/Create/Dandy Dino/Modulate!/Create New Game";
+        public const string CREATE_MODULE = "Assets/Create/Dandy Dino/Modulate!/Create New Module";
     }
 }
